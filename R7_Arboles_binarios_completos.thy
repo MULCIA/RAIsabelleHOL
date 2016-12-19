a9580bda4243c0d14453b821415020ac2002df16
serrodcal <sergiorodriguezcalvo@gmail.com>
2016-12-19 08:00:52 +0100

    Solve exercise 7 R7

--- R7_Arboles_binarios_completos.thy
+++ R7_Arboles_binarios_completos.thy
@@ -140,6 +140,9 @@ lemma arbol_completo_respecto_num_hojas:
 using assms
 by (induct n) auto
 
+lemma lej8: "es_abc hojas a = es_abc size a"
+by (induct a) (auto simp add:arbol_completo_respecto_num_hojas [symmetric])
+
 text {*  
   --------------------------------------------------------------------- 
   Ejercicio 9. Demostrar que un árbol binario a es completo respecto de
@@ -148,7 +151,7 @@ text {*
 *}
 
 lemma arbol_completo_respecto_profundidad: "es_abc profundidad n = es_abc size n"
-by (simp add: arbol_profundidad_respecto_num_hojas arbol_completo_respecto_num_hojas) 
+by (simp add: lej7 lej8) 
 
 text {*  
   --------------------------------------------------------------------- 
