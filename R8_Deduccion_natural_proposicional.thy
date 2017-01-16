9f43f895e610dfce4b999762160b095e7241a1e3
serrodcal <sergiorodriguezcalvo@gmail.com>
2017-01-16 10:59:14 +0100

    Refactor by isabelle2016

--- R8_Deduccion_natural_proposicional.thy
+++ R8_Deduccion_natural_proposicional.thy
@@ -8,76 +8,76 @@ text {*
   Demostrar o refutar los siguientes lemas usando sólo las reglas
   básicas de deducción natural de la lógica proposicional, de los
   cuantificadores y de la igualdad: 
-  · conjI:      ⟦P; Q⟧ ⟹ P ∧ Q
-  · conjunct1:  P ∧ Q ⟹ P
-  · conjunct2:  P ∧ Q ⟹ Q  
-  · notnotD:    ¬¬ P ⟹ P
-  · mp:         ⟦P ⟶ Q; P⟧ ⟹ Q 
-  · impI:       (P ⟹ Q) ⟹ P ⟶ Q
-  · disjI1:     P ⟹ P ∨ Q
-  · disjI2:     Q ⟹ P ∨ Q
-  · disjE:      ⟦P ∨ Q; P ⟹ R; Q ⟹ R⟧ ⟹ R 
-  · FalseE:     False ⟹ P
-  · notE:       ⟦¬P; P⟧ ⟹ R
-  · notI:       (P ⟹ False) ⟹ ¬P
-  · iffI:       ⟦P ⟹ Q; Q ⟹ P⟧ ⟹ P = Q
-  · iffD1:      ⟦Q = P; Q⟧ ⟹ P 
-  · iffD2:      ⟦P = Q; Q⟧ ⟹ P
-  · ccontr:     (¬P ⟹ False) ⟹ P
+  · conjI:      \<lbrakk>P; Q\<rbrakk> \<Longrightarrow> P \<and> Q
+  · conjunct1:  P \<and> Q \<Longrightarrow> P
+  · conjunct2:  P \<and> Q \<Longrightarrow> Q  
+  · notnotD:    \<not>\<not> P \<Longrightarrow> P
+  · mp:         \<lbrakk>P \<longrightarrow> Q; P\<rbrakk> \<Longrightarrow> Q 
+  · impI:       (P \<Longrightarrow> Q) \<Longrightarrow> P \<longrightarrow> Q
+  · disjI1:     P \<Longrightarrow> P \<or> Q
+  · disjI2:     Q \<Longrightarrow> P \<or> Q
+  · disjE:      \<lbrakk>P \<or> Q; P \<Longrightarrow> R; Q \<Longrightarrow> R\<rbrakk> \<Longrightarrow> R 
+  · FalseE:     False \<Longrightarrow> P
+  · notE:       \<lbrakk>\<not>P; P\<rbrakk> \<Longrightarrow> R
+  · notI:       (P \<Longrightarrow> False) \<Longrightarrow> \<not>P
+  · iffI:       \<lbrakk>P \<Longrightarrow> Q; Q \<Longrightarrow> P\<rbrakk> \<Longrightarrow> P = Q
+  · iffD1:      \<lbrakk>Q = P; Q\<rbrakk> \<Longrightarrow> P 
+  · iffD2:      \<lbrakk>P = Q; Q\<rbrakk> \<Longrightarrow> P
+  · ccontr:     (\<not>P \<Longrightarrow> False) \<Longrightarrow> P
  
-  · allI:       ⟦∀x. P x; P x ⟹ R⟧ ⟹ R
-  · allE:       (⋀x. P x) ⟹ ∀x. P x
-  · exI:        P x ⟹ ∃x. P x
-  · exE:        ⟦∃x. P x; ⋀x. P x ⟹ Q⟧ ⟹ Q
+  · allI:       \<lbrakk>\<forall>x. P x; P x \<Longrightarrow> R\<rbrakk> \<Longrightarrow> R
+  · allE:       (\<And>x. P x) \<Longrightarrow> \<forall>x. P x
+  · exI:        P x \<Longrightarrow> \<exists>x. P x
+  · exE:        \<lbrakk>\<exists>x. P x; \<And>x. P x \<Longrightarrow> Q\<rbrakk> \<Longrightarrow> Q
  
   · refl:       t = t
-  · subst:      ⟦s = t; P s⟧ ⟹ P t
-  · trans:      ⟦r = s; s = t⟧ ⟹ r = t
-  · sym:        s = t ⟹ t = s
-  · not_sym:    t ≠ s ⟹ s ≠ t
-  · ssubst:     ⟦t = s; P s⟧ ⟹ P t
-  · box_equals: ⟦a = b; a = c; b = d⟧ ⟹ a: = d
-  · arg_cong:   x = y ⟹ f x = f y
-  · fun_cong:   f = g ⟹ f x = g x
-  · cong:       ⟦f = g; x = y⟧ ⟹ f x = g y
+  · subst:      \<lbrakk>s = t; P s\<rbrakk> \<Longrightarrow> P t
+  · trans:      \<lbrakk>r = s; s = t\<rbrakk> \<Longrightarrow> r = t
+  · sym:        s = t \<Longrightarrow> t = s
+  · not_sym:    t \<noteq> s \<Longrightarrow> s \<noteq> t
+  · ssubst:     \<lbrakk>t = s; P s\<rbrakk> \<Longrightarrow> P t
+  · box_equals: \<lbrakk>a = b; a = c; b = d\<rbrakk> \<Longrightarrow> a: = d
+  · arg_cong:   x = y \<Longrightarrow> f x = f y
+  · fun_cong:   f = g \<Longrightarrow> f x = g x
+  · cong:       \<lbrakk>f = g; x = y\<rbrakk> \<Longrightarrow> f x = g y
 *}
  
 text {*
   Se usarán las reglas notnotI, mt y not_ex que demostramos a continuación.
   *}
  
-lemma notnotI: "P ⟹ ¬¬ P"
+lemma notnotI: "P \<Longrightarrow> \<not>\<not> P"
 by auto
  
-lemma mt: "⟦F ⟶ G; ¬G⟧ ⟹ ¬F"
+lemma mt: "\<lbrakk>F \<longrightarrow> G; \<not>G\<rbrakk> \<Longrightarrow> \<not>F"
 by auto
  
-lemma no_ex: "¬(∃x. P(x)) ⟹ ∀x. ¬P(x)"
+lemma no_ex: "\<not>(\<exists>x. P(x)) \<Longrightarrow> \<forall>x. \<not>P(x)"
 by auto
  
 text {* --------------------------------------------------------------- 
   Ejercicio 1. Demostrar
-     ¬q ⟶ ¬p ⊢ p ⟶ q
+     \<not>q \<longrightarrow> \<not>p \<turnstile> p \<longrightarrow> q
   ------------------------------------------------------------------ *}
  
 text {* --------------------------------------------------------------- 
   Ejercicio 2. Demostrar
-     ¬(¬p ∧ ¬q) ⊢ p ∨ q
+     \<not>(\<not>p \<and> \<not>q) \<turnstile> p \<or> q
   ------------------------------------------------------------------ *}
  
 text {* --------------------------------------------------------------- 
   Ejercicio 3. Demostrar
-     ¬(¬p ∨ ¬q) ⊢ p ∧ q
+     \<not>(\<not>p \<or> \<not>q) \<turnstile> p \<and> q
   ------------------------------------------------------------------ *}
  
 text {* --------------------------------------------------------------- 
   Ejercicio 4. Demostrar
-     ¬(p ∧ q) ⊢ ¬p ∨ ¬q
+     \<not>(p \<and> q) \<turnstile> \<not>p \<or> \<not>q
   ------------------------------------------------------------------ *}
  
 text {* --------------------------------------------------------------- 
   Ejercicio 5. Demostrar
-     ⊢ (p ⟶ q) ∨ (q ⟶ p)
+     \<turnstile> (p \<longrightarrow> q) \<or> (q \<longrightarrow> p)
   ------------------------------------------------------------------ *}
  
 end
